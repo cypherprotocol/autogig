9673d7abc69b506e43d46ab8f51ab1de1cff0091
soju <underrated@gmail.com>
2023-07-13 12:37:00 -0700

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import "@rainbow-me/rainbowkit/styles.css";
 import "../styles/globals.css";
 import "../styles/tailwind.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Autogig",
@@ -27,6 +28,7 @@ export default function RootLayout({
           </div>
         </Provider>
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
