ac615f4505b54a0dd212ee08657da04f22bd46a7
soju <underrated@gmail.com>
2023-08-05 12:07:39 -0700

    enable subscribe route

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/api/subscribe"],
 });
 
 // make sure the middleware only runs when
