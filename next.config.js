4067d9442e125f3ac3bfddb10de6b9c205d95a80
soju <underrated@gmail.com>
2023-08-10 20:47:24 -0700

    try middleware

--- next.config.js
+++ next.config.js
@@ -4,38 +4,12 @@ module.exports = {
   },
   experimental: { appDir: true, serverActions: true },
   transpilePackages: ["crawlee"],
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/ingest/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
   async redirects() {
     return [
       {
         source: "/contact",
         destination: "https://discord.gg/j4BAHXm77",
         permanent: false,
-      },
-      {
-        source: "/ingest/:path*",
-        destination: "https://app.posthog.com",
-        permanent: true,
       },
     ];
   },
