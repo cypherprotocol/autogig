55ab857d9a52a950273d7e5d125ac3d92a49dd50
soju <underrated@gmail.com>
2023-08-10 22:15:48 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,40 +4,6 @@ module.exports = {
   },
   experimental: { appDir: true, serverActions: true },
   transpilePackages: ["crawlee"],
-  async headers() {
-    return [
-      {
-        source: "/ingest/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
-  async redirects() {
-    return [
-      {
-        source: "/contact",
-        destination: "https://discord.gg/j4BAHXm77",
-        permanent: false,
-      },
-      {
-        source: "/ingest/:path*",
-        destination: "app.posthog.com",
-        permanent: false,
-      },
-    ];
-  },
   // webpack: (config) => {
   //   config.resolve.fallback = {
   //     fs: false,
