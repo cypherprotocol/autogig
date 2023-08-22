ae08c9c9c7f92f7ed49a55670e082c2413aae114
soju <underrated@gmail.com>
2023-08-21 22:45:39 -0700

    Update page.tsx

--- app/[locale]/find/page.tsx
+++ app/[locale]/find/page.tsx
@@ -161,7 +161,7 @@ export default function Home() {
                           initial={{ opacity: 0, y: -10 }}
                           animate={{ opacity: 1, y: 0 }}
                           exit={{ opacity: 0, y: 10 }}
-                          transition={{ duration: 0.5 }}
+                          transition={{ duration: 0.3 }}
                         >
                           {fakeLoadingText[currentIndex]}
                         </motion.h3>
