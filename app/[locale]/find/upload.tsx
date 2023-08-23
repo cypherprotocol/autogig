c22ff7f5fdd75f76a12ac37766d1e54e600c2f17
soju <underrated@gmail.com>
2023-08-23 11:28:37 -0700

    fix upload

--- app/[locale]/find/upload.tsx
+++ app/[locale]/find/upload.tsx
@@ -29,6 +29,7 @@ import Image from "next/image";
 import { usePostHog } from "posthog-js/react";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { useDropzone } from "react-dropzone";
+import useDrivePicker from "react-google-drive-picker";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 
@@ -52,13 +53,13 @@ export function Upload() {
 
   const onDrop = useCallback((acceptedFiles) => {
     setResume(acceptedFiles?.[0]);
+    console.log(acceptedFiles);
     // Do something with the files
     setIsFileUploaded(true);
   }, []);
 
   const { getRootProps, getInputProps, isDragActive, open } = useDropzone({
     onDrop,
-    noClick: true,
     // noKeyboard: true,
   });
 
@@ -98,6 +99,51 @@ export function Upload() {
       window.removeEventListener("resize", checkMobile);
     };
   }, []);
+
+  const [openPicker, authResponse] = useDrivePicker();
+  // const customViewsArray = [new google.picker.DocsView()]; // custom view
+  const handleOpenPicker = () => {
+    openPicker({
+      clientId:
+        "340519380755-hqpq4uu3svr502kbuudd0p5hkk3as0rq.apps.googleusercontent.com",
+      developerKey: "AIzaSyD8DLOQG9agNQiD2lei5EysQqz7tCF1D-g",
+      viewId: "PDFS",
+      token:
+        "ya29.a0AfB_byBK6k15RZSu9mqXGboZ4Rv2mEjfSsP_6gxjFHXM1j090KaMWbuwUD_gBukqE7hrTWTFQIkCX98NMXOw0cVNSCcWZnuawzVliqOZxUq0Q4ed0pZBcxq4fn6lwh9H99Hs47bqE9YT1rjDuEGVZo3nVwpcKczAEEXw5QaCgYKAXoSARMSFQHsvYlsvBvFG14yKtQ5uagw8t8VEA0173", // pass oauth token in case you already have one
+      showUploadView: true,
+      showUploadFolders: true,
+      supportDrives: true,
+      // customViews: customViewsArray, // custom view
+      callbackFunction: (data) => {
+        if (data.action === "cancel") {
+          console.log("User clicked cancel/close button");
+        }
+
+        if (data?.docs?.[0]?.id) {
+          let fileId = data.docs[0].id;
+          fetch(`https://www.googleapis.com/drive/v3/files/${fileId}`, {
+            headers: new Headers({
+              Authorization:
+                "Bearer " +
+                "a0AfB_byBK6k15RZSu9mqXGboZ4Rv2mEjfSsP_6gxjFHXM1j090KaMWbuwUD_gBukqE7hrTWTFQIkCX98NMXOw0cVNSCcWZnuawzVliqOZxUq0Q4ed0pZBcxq4fn6lwh9H99Hs47bqE9YT1rjDuEGVZo3nVwpcKczAEEXw5QaCgYKAXoSARMSFQHsvYlsvBvFG14yKtQ5uagw8t8VEA0173",
+            }),
+          })
+            .then((response) => response.blob())
+            .then((blob) => {
+              let file = new File([blob], data.docs[0].name, {
+                type: "application/pdf",
+              });
+              console.log(file);
+              setResume(file);
+              setIsFileUploaded(true);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      },
+    });
+  };
 
   return (
     <div className="flex w-full flex-col items-center justify-center px-4">
@@ -198,6 +244,22 @@ export function Upload() {
                         </Button>
                       </div>
                     )}
+                    {/* <Button
+                      type="button"
+                      onClick={handleOpenPicker}
+                      className="mt-4 w-full"
+                    >
+                      <svg
+                        role="img"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="mr-4 h-4 w-4 fill-white"
+                      >
+                        <title>Google Drive</title>
+                        <path d="M12.01 1.485c-2.082 0-3.754.02-3.743.047.01.02 1.708 3.001 3.774 6.62l3.76 6.574h3.76c2.081 0 3.753-.02 3.742-.047-.005-.02-1.708-3.001-3.775-6.62l-3.76-6.574zm-4.76 1.73a789.828 789.861 0 0 0-3.63 6.319L0 15.868l1.89 3.298 1.885 3.297 3.62-6.335 3.618-6.33-1.88-3.287C8.1 4.704 7.255 3.22 7.25 3.214zm2.259 12.653-.203.348c-.114.198-.96 1.672-1.88 3.287a423.93 423.948 0 0 1-1.698 2.97c-.01.026 3.24.042 7.222.042h7.244l1.796-3.157c.992-1.734 1.85-3.23 1.906-3.323l.104-.167h-7.249z" />
+                      </svg>
+                      Upload from Google Drive
+                    </Button> */}
                   </>
                 ) : (
                   <Alert>
@@ -288,7 +350,7 @@ export function Upload() {
 
           <Button
             type="button"
-            onClick={open}
+            onClick={onSubmit}
             disabled={
               (option === "resume" && !isFileUploaded) ||
               (option === "github" && !form.formState.isValid)
