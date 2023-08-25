913bea87cc7c2e3b8be26e4cfd35e13b26ccd4dd
soju <underrated@gmail.com>
2023-08-25 12:10:10 -0700

    Update data-table-faceted-filter.tsx

--- components/data-table-faceted-filter.tsx
+++ components/data-table-faceted-filter.tsx
@@ -113,9 +113,9 @@ export function DataTableFacetedFilter<TData, TValue>({
                     >
                       <CheckIcon className={cn("h-4 w-4")} />
                     </div>
-                    {option.icon && (
+                    {/* {option.icon && (
                       <option.icon className="mr-2 h-4 w-4 text-muted-foreground" />
-                    )}
+                    )} */}
                     <span>{option.label}</span>
                     {facets?.get(option.value) && (
                       <span className="ml-auto flex h-4 w-4 items-center justify-center font-mono text-xs">
