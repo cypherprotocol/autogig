b67554cc9c6a713cf54913beecbebb5c3e144dac
soju <underrated@gmail.com>
2023-08-26 12:18:08 -0700

    add adblock detect

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,16 +3,37 @@
 import Navbar from "@/app/navbar";
 import Offer from "@/components/offer";
 import { HOME_BUCKETS } from "@/lib/buckets";
+import { useDetectAdBlock } from "adblock-detect-react";
+import Image from "next/image";
 
 export async function generateStaticParams() {
   return HOME_BUCKETS.map((group) => ({ bucket: group }));
 }
 
 export default function Home() {
+  const adBlockDetected = useDetectAdBlock();
+
   return (
     <>
-      <Navbar />
-      <Offer />
+      {adBlockDetected ? (
+        <>
+          <Image
+            src="/fullcolor-retro-dudes-lock.svg"
+            width={200}
+            height={200}
+            alt=""
+            className="mb-4"
+          />
+          <h3 className="mb-8 scroll-m-20 text-center text-2xl font-semibold tracking-tight">
+            Please disable your adblocker to continue using Autogig.
+          </h3>
+        </>
+      ) : (
+        <>
+          <Navbar />
+          <Offer />
+        </>
+      )}
     </>
   );
 }
--- components/data-table-row-actions.tsx
+++ components/data-table-row-actions.tsx
@@ -1,25 +1,10 @@
 "use client";
 
-import { DotsHorizontalIcon } from "@radix-ui/react-icons";
 import { Row } from "@tanstack/react-table";
 
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 
 import Link from "next/link";
-import { labels } from "../data/data";
 import { taskSchema } from "../data/schema";
 
 interface DataTableRowActionsProps<TData> {
@@ -32,39 +17,44 @@ export function DataTableRowActions<TData>({
   const task = taskSchema.parse(row.original);
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="ghost"
-          className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
-        >
-          <DotsHorizontalIcon className="h-4 w-4" />
-          <span className="sr-only">Open menu</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-[160px]">
-        <Link href={`/dashboard/task/${task.id}`}>
-          <DropdownMenuItem>Details</DropdownMenuItem>
-        </Link>
-        <DropdownMenuSeparator />
-        <DropdownMenuSub>
-          <DropdownMenuSubTrigger>Labels</DropdownMenuSubTrigger>
-          <DropdownMenuSubContent>
-            <DropdownMenuRadioGroup value={task.label}>
-              {labels.map((label) => (
-                <DropdownMenuRadioItem key={label.value} value={label.value}>
-                  {label.label}
-                </DropdownMenuRadioItem>
-              ))}
-            </DropdownMenuRadioGroup>
-          </DropdownMenuSubContent>
-        </DropdownMenuSub>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          Delete
-          <DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <Link href={`/dashboard/task/${task.id}`}>
+        <Button>Details</Button>
+      </Link>
+    </>
+    // <DropdownMenu>
+    //   <DropdownMenuTrigger asChild>
+    //     <Button
+    //       variant="ghost"
+    //       className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+    //     >
+    //       <DotsHorizontalIcon className="h-4 w-4" />
+    //       <span className="sr-only">Open menu</span>
+    //     </Button>
+    //   </DropdownMenuTrigger>
+    //   <DropdownMenuContent align="end" className="w-[160px]">
+    //     <Link href={`/dashboard/task/${task.id}`}>
+    //       <DropdownMenuItem>Details</DropdownMenuItem>
+    //     </Link>
+    //     <DropdownMenuSeparator />
+    //     <DropdownMenuSub>
+    //       <DropdownMenuSubTrigger>Labels</DropdownMenuSubTrigger>
+    //       <DropdownMenuSubContent>
+    //         <DropdownMenuRadioGroup value={task.label}>
+    //           {labels.map((label) => (
+    //             <DropdownMenuRadioItem key={label.value} value={label.value}>
+    //               {label.label}
+    //             </DropdownMenuRadioItem>
+    //           ))}
+    //         </DropdownMenuRadioGroup>
+    //       </DropdownMenuSubContent>
+    //     </DropdownMenuSub>
+    //     <DropdownMenuSeparator />
+    //     <DropdownMenuItem>
+    //       Delete
+    //       <DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
+    //     </DropdownMenuItem>
+    //   </DropdownMenuContent>
+    // </DropdownMenu>
   );
 }
